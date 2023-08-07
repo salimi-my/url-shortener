0ca29a760ce198a6d4a8fe07c90b9a3b60ba8e15
Mohamad Salimi <contact@salimi.my>
2023-08-07 23:06:24 +0800

    Update shadcn/ui theme to new york

--- app/(admin)/admin/(routes)/page.tsx
+++ app/(admin)/admin/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
     <div>
       <UserButton afterSignOutUrl='/' />
+      <Button className='ml-2'>Button</Button>
     </div>
   );
 };
