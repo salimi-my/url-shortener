a3d873cfc971bec1272ea26887002610d6ee79a2
Mohamad Salimi <contact@salimi.my>
2023-08-09 00:29:42 +0800

    Add sign out button

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,7 +5,8 @@ import { usePathname } from 'next/navigation';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
-import { LayoutGrid, List, User } from 'lucide-react';
+import { LayoutGrid, List, LogOut, User } from 'lucide-react';
+import { SignOutButton } from '@clerk/nextjs';
 
 export function MainNav({
   className,
@@ -38,16 +39,28 @@ export function MainNav({
     <nav className='mt-8'>
       <ul className={cn('flex flex-col items-start space-y-1 px-2', className)}>
         {routes.map((route) => (
-          <Link className='w-full' key={route.href} href={route.href}>
-            <Button
-              variant={route.active ? 'secondary' : 'ghost'}
-              className='w-full justify-start h-10'
-            >
-              <span className='mr-2'>{route.icon}</span>
-              {route.label}
-            </Button>
-          </Link>
+          <li className='w-full' key={route.href}>
+            <Link href={route.href}>
+              <Button
+                variant={route.active ? 'secondary' : 'ghost'}
+                className='w-full justify-start h-10'
+              >
+                <span className='mr-4'>{route.icon}</span>
+                {route.label}
+              </Button>
+            </Link>
+          </li>
         ))}
+        <li className='w-full'>
+          <SignOutButton>
+            <Button variant='ghost' className='w-full justify-start h-10'>
+              <span className='mr-4'>
+                <LogOut size={18} />
+              </span>
+              Sign Out
+            </Button>
+          </SignOutButton>
+        </li>
       </ul>
     </nav>
   );
