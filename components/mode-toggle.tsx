4eac89359a78d2697069a093cb4e213df942d66a
Mohamad Salimi <contact@salimi.my>
2023-08-20 12:50:12 +0800

    Add transition animation

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -35,8 +35,8 @@ export function ModeToggle({ float }: ToggleProps) {
               asChild
             >
               <Button className='rounded-full' variant='ghost' size='icon'>
-                <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-                <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+                <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0' />
+                <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100' />
                 <span className='sr-only'>Switch Theme</span>
               </Button>
             </DropdownMenuTrigger>
